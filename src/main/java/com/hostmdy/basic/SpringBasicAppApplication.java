ad1ae6d9c2a1bb96a31aeca3d923e4c302884075
yanlinhtoo <challengeauto57@gmail.com>
2022-10-26 23:55:13 +0630

    Initila commit for spring_basic_app

---
+++ src/main/java/com/hostmdy/basic/SpringBasicAppApplication.java
@@ -0,0 +1,13 @@
+package com.hostmdy.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicAppApplication.class, args);
+	}
+
+}
